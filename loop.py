685df158c65d43c5b9247442147b9d5691eed9d9
licheeee <qiaoxw@outlook.com>
2019-02-16 13:56:43 +0800

    循环

---
+++ loop.py
@@ -0,0 +1,19 @@
+# coding : utf-8
+
+# 简单的循环
+
+# while可以跟else
+
+#for 循环
+
+print ("for ... in range(...)")
+for i in range(10) :
+    print (i, end = ',' )
+
+print ()
+
+print ('for ... in list')
+list = ['baidu', 'google', 'apple', 'alibaba']
+
+for i in list :
+    print (i)
